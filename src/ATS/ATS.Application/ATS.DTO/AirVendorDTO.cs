0710981ff52777d34ee2996ae3ce7da4189934a3
Shivendra Aggarwal <shivendra.aggarwal@outlook.com>
2019-02-26 19:30:20 +0530

    added required data transmition objects

--- src/ATS/ATS.Application/ATS.DTO/AirVendorDTO.cs
+++ src/ATS/ATS.Application/ATS.DTO/AirVendorDTO.cs
@@ -6,10 +6,16 @@ namespace ATS.DTO
 {
     public class AirVendorDTO
     {
-        public int AirVendorId { get; set; }
+        public int Id { get; set; }
 
         public string AccessUrl { get; set; }
 
+        public string VendorName { get; set; }
+
         public bool AvailabilityStatus { get; set; }
+
+        public string AvailabilityTestUrl { get; set; }
+
+        public ICollection<SeatDTO> Seats { get; set; }
     }
 }
--- src/ATS/ATS.Application/ATS.DTO/SeatDTO.cs
+++ src/ATS/ATS.Application/ATS.DTO/SeatDTO.cs
@@ -4,6 +4,22 @@ namespace ATS.DTO
 {
     public class SeatDTO
     {
+        public Guid ExternalId { get; set; }
 
+        public DateTime AvailableDate { get; set; }
+
+        public int BasePrice { get; set; }
+
+        public string FromCityName { get; set; }
+
+        public string ToCityName { get; set; }
+
+        public string FlightNumber { get; set; }
+
+        public string SeatType { get; set; }
+
+        public int SeatNumber { get; set; }
+        
+        public string VendorName { get; set; }
     }
 }
